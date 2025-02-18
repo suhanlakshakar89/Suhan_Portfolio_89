0a4fc5a3e3ea0cb3838fb7fa9ffa30b31b9cf43a
suhanlakshakar89 <suhanlakshakar@gmail.com>
2025-02-18 23:31:36 +0530

    Fixed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-// import Callimage from '/public/icons8-call-50.png';
-// import Mailimage2 from '/public/icons8-letter.gif';
+import Callimage from '../assets/icons8-call-50.png'
+import Mailimage2 from '../assets/icons8-letter.gif'
 
 
 function Footer() {
@@ -11,11 +11,12 @@ function Footer() {
 
         <div className="flex flex-col md:flex-row items-center gap-4 mb-4 md:mb-0">
 
-        {/* <img src={Callimage} alt="w-3 h-3 mr-2" /> */}
+        <img src={Callimage} alt="w-3 h-3 mr-2" />
           <a href="tel:8302181931" className="text-cyan-400 hover:text-blue-600"> 
           Phone : 8302181931
           </a>
-          {/* <img src={Mailimage2} alt="MailGif" className='w-5 h-5 mr-2' /> */}
+          
+          <img src={Mailimage2} alt="MailGif" className='w-5 h-5 mr-2' />
           <a href="mailto:suhanlakshakar3222@gmail.com" className="text-cyan-400 hover:text-blue-600"> 
             Email: suhanlakshakar3222@gmail.com
           </a>
